d0acc4617f592e0d7dd06c53f7f271cabecfdb61
VisakanMathy <visakan.mathivannan@emotech.co>
2020-12-01 19:35:11 +0000

    update

--- my-app/src/pages/ProjectPage.tsx
+++ my-app/src/pages/ProjectPage.tsx
@@ -5,21 +5,6 @@ import Content from "../CMS.json";
 import ImageGallery from "react-image-gallery";
 import ReactPlayer from "react-player/";
 
-const images = [
-  {
-    original: "https://picsum.photos/id/1018/1000/600/",
-    thumbnail: "https://picsum.photos/id/1018/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1015/1000/600/",
-    thumbnail: "https://picsum.photos/id/1015/250/150/",
-  },
-  {
-    original: "https://picsum.photos/id/1019/1000/600/",
-    thumbnail: "https://picsum.photos/id/1019/250/150/",
-  },
-];
-
 interface ProjectPageProps {
   page: Pages;
   setPage: (Page: Pages) => void;
