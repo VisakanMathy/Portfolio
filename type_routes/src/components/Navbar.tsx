523cd1981657ed733fd4940f728c209fd697432c
VisakanMathy <visakan.mathivannan@emotech.co>
2020-12-17 19:46:07 +0000

    fix errors

--- type_routes/src/components/Navbar.tsx
+++ type_routes/src/components/Navbar.tsx
@@ -2,28 +2,26 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./css/NavBar.css";
 
-
 export default function NavBar() {
-    return (
-      <div className="NavBar">
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/projects">Projects</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
-        <div className="ButtonGroup"></div>
-      </div>
-    );
-  }
+  return (
+    <div className="NavBar">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/projects">Projects</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+      <div className="ButtonGroup"></div>
+    </div>
+  );
 }
